fbc926d3a1d48d343a1881d6818643658ed3530f
5946 <saucara5946@gmail.com>
2026-06-02 14:07:03 +0800

    后端触发器

--- shujuku/app/app.py
+++ shujuku/app/app.py
@@ -15,3 +15,19 @@ def get_connection():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/insert_album_song', methods=['POST'])
+def insert_album_song():
+    album_id = request.form.get('album_id')
+    song_id = request.form.get('song_id')
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO album_song VALUES (%s, %s)", (album_id, song_id))
+        conn.commit()
+        return jsonify({'status': 'success', 'message': f'Successfully added song {song_id} to album {album_id}!'})
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)})
+    finally:
+        conn.close()
