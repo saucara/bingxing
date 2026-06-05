609917db58e1e7a5c726c43f7e607f8c570371e1
5946 <saucara5946@gmail.com>
2026-06-05 22:21:07 +0800

    改

--- shujuku/basic/data/insert_data.sql
+++ shujuku/basic/data/insert_data.sql
@@ -62,3 +62,40 @@ INSERT INTO setlist VALUES
 (1,1),(1,2),(1,3),(1,4),(1,5),
 (2,2),(2,3),(2,4),
 (3,1),(3,3);
+
+--补充
+INSERT INTO song VALUES
+(6, 'Truth', 258, '2008-09-03', 'theme', NULL),
+(7, 'One Love', 312, '2008-07-23', 'theme', NULL),
+(8, 'Troublemaker', 243, '2009-02-04', 'theme', NULL),
+(9, 'Crazy Moon', 261, '2009-07-01', 'theme', NULL),
+(10, 'Believe', 278, '2009-01-21', 'theme', NULL),
+(11, 'Lotus', 294, '2011-11-02', 'theme', NULL),
+(12, 'Face Down', 267, '2012-05-02', 'theme', NULL),
+(13, 'Calling', 289, '2012-05-02', 'theme', NULL),
+(14, 'Breathless', 271, '2012-08-01', 'theme', NULL),
+(15, 'Endless Game', 304, '2013-04-03', 'theme', NULL),
+(16, 'Lucky Man', 258, '2013-11-06', 'theme', NULL),
+(17, 'Bittersweet', 245, '2014-01-29', 'theme', NULL),
+(18, 'Hope in the Darkness', 312, '2014-09-03', 'theme', NULL),
+(19, 'Sugar', 234, '2015-02-04', 'theme', NULL),
+(20, 'Sakumoto', 267, '2016-06-08', 'theme', NULL);
+
+INSERT INTO original_song VALUES
+(6, 'Hayashi Tetsuji', 'Kitade Hajime', 'Truth', 258, '2008-09-03', 'theme', NULL),
+(7, 'Machida Hiroshi', 'Aoki Takaaki', 'One Love', 312, '2008-07-23', 'theme', NULL),
+(8, 'Fuyumi Ono', 'D.A.I', 'Troublemaker', 243, '2009-02-04', 'theme', NULL),
+(9, 'Matsui Goro', 'Sekine Yasuhiko', 'Crazy Moon', 261, '2009-07-01', 'theme', NULL),
+(10, 'Machida Hiroshi', 'Aoki Takaaki', 'Believe', 278, '2009-01-21', 'theme', NULL);
+
+INSERT INTO setlist VALUES
+(1,6),(1,7),(1,8),(1,9),(1,10),
+(1,11),(1,12),(1,13),(1,14),(1,15),
+(1,16),(1,17),(1,18),(1,19),(1,20);
+
+INSERT INTO setlist VALUES
+(2,6),(2,7),(2,8),(2,10),
+(2,11),(2,12),(2,15),(2,17);
+
+INSERT INTO setlist VALUES
+(3,6),(3,7),(3,8),(3,10);
